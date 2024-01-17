735d63b90be687247d667b2dd68a982d49b39310
Darshan2348 <156394680+Darshan2348@users.noreply.github.com>
2024-01-17 09:55:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,11 +76,11 @@ REQ_CHANNEL = int(environ.get('REQ_CHANNEL'))
 # LAZY_AI_LOGS = int(environ.get("LAZY_AI_LOGS","")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of LazyPrincess ]
 
 # Requested Content template variables ---
-ADMIN_USRNM = environ.get('ADMIN_USRNM','SPOfficialSupport_bot') # WITHOUT @
+ADMIN_USRNM = environ.get('ADMIN_USRNM','Sharathp23') # WITHOUT @
 MAIN_CHANNEL_USRNM = environ.get('MAIN_CHANNEL_USRNM','sp_official_movies') # WITHOUT @
 DEV_CHANNEL_USRNM = environ.get('DEV_CHANNEL_USRNM','sp_official_movies') # WITHOUT @
 LAZY_YT_HANDLE = environ.get('LAZY_YT_HANDLE','SPOFFICIALINFO')  # WITHOUT @ [  add only handle - don't add full url  ] 
-MOVIE_GROUP_USERNAME = environ.get('MOVIE_GROUP_USERNAME', "+ew2leC-96JozNzY1") #[ without @ ]
+MOVIE_GROUP_USERNAME = environ.get('MOVIE_GROUP_USERNAME', "+OaQgwV3cym4yNTNI") #[ without @ ]
 
 # Url Shortner
 URL_MODE = is_enabled((environ.get("URL_MODE","True")), False)
